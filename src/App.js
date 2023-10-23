6c5f395853a019779c4aacca8fb2a6b3f4e6a0c0
Phattarapong66 <137135183+Phattarapong66@users.noreply.github.com>
2023-10-23 21:18:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [data, setData] = useState();
+  return (
+    <Container>
+      <h4>React Hook Form</h4>
+      <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            {...register("email", {
+              required: "required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "รูปแบบของอีเมล์ไม่ถูกต้อง",
+              },
+            })}
+            type="email"
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            {...register("password", {
+              required: "required",
+              minLength: {
+                value: 8,
+                message: "รหัสผ่านต้องมีความยาว 8 ตัวอักษรขึ้นไป",
+              },
+            })}
+            type="password"
+            placeholder="Enter password"
+          />
+          <Form.Text className="text-danger">
+            {errors.password?.message}
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>ประเภทสมาชิก</Form.Label>
+          <Form.Select {...register("userType")}>
+            <option value="1" selected>
+              ทั่วไป
+            </option>
+            <option value="2">VIP</option>
+            <option value="3">ผู้ดูแลระบบ</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            {...register("remember")}
+            type="checkbox"
+            label="Remember me"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Text>{data}</Form.Text>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
